174d265c8a1d5c20d9d4041164be6569c4bb87eb
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-26 02:37:49 -0600

    Added new event deliver_products

--- consumers.py
+++ consumers.py
@@ -26,8 +26,21 @@ def pickup_products(state, event):
         "status": "collected"
     }
 
+def deliver_products(state, event):
+    data = json.loads(event.data)
+    new_budget = state['budget'] + int(data['sell_price']) * int(data['quantity'])
+    new_quantity = state['quantity'] - int(data['quantity'])
+
+    return state | {
+        "budget": new_budget,
+        "sell_price": int(data['sell_price']),
+        "quantity": new_quantity,
+        "status": "collected"
+    }
+
 CONSUMERS = {
     "CREATE_DELIVERY": create_delivery,
     "START_DELIVERY": start_delivery,
-    "PICKUP_PRODUCTS": pickup_products
+    "PICKUP_PRODUCTS": pickup_products,
+    "DELIVER_PRODUCTS": deliver_products,
 }
